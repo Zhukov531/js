f9cfa8d32351438eee420a5a86e8c6d93023b03d
Zhukov531 <zhukov531@gmail.com>
2022-01-01 17:06:31 +0300

    Hey problem

--- js/Script.js
+++ js/Script.js
@@ -1,1 +1,15 @@
-console.log('Hello world');
+//"use strict";
+
+console.log('Zhukov Yuriy');
+
+let name = prompt('Введите ваше имя', "Джон");
+alert(`Вас зовут ${name}`);
+
+let year = prompt("В каком году я родился?", "");
+if (year < 2005){
+    alert("Слишком рано");} 
+else if (year > 2005) {
+    alert("Слишком поздно");}
+else{
+    alert("Самое то!");}
+
